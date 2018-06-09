48b74267b9a2abd88e5067ee9ad34cb2513ce331
DariusHamlyn <39625359+DariusHamlyn@users.noreply.github.com>
2018-06-09 10:22:30 +0700

    Add files via upload

---
+++ FrmTambahData.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace AddressBookBetter
+{
+    partial class FrmTambahData
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.txtAlamat = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtKota = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNoHp = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dtpTglLahir = new System.Windows.Forms.DateTimePicker();
+            this.btnSimpan = new System.Windows.Forms.Button();
+            this.btnBatal = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nama";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtNama
+            // 
+            this.txtNama.Location = new System.Drawing.Point(62, 14);
+            this.txtNama.Margin = new System.Windows.Forms.Padding(2);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(181, 20);
+            this.txtNama.TabIndex = 0;
+            this.txtNama.TextChanged += new System.EventHandler(this.txtNama_TextChanged);
+            // 
+            // txtAlamat
+            // 
+            this.txtAlamat.Location = new System.Drawing.Point(62, 37);
+            this.txtAlamat.Margin = new System.Windows.Forms.Padding(2);
+            this.txtAlamat.Name = "txtAlamat";
+            this.txtAlamat.Size = new System.Drawing.Size(347, 20);
+            this.txtAlamat.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 39);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Alamat";
+            // 
+            // txtKota
+            // 
+            this.txtKota.Location = new System.Drawing.Point(62, 61);
+            this.txtKota.Margin = new System.Windows.Forms.Padding(2);
+            this.txtKota.Name = "txtKota";
+            this.txtKota.Size = new System.Drawing.Size(135, 20);
+            this.txtKota.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 62);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Kota";
+            // 
+            // txtNoHp
+            // 
+            this.txtNoHp.Location = new System.Drawing.Point(62, 82);
+            this.txtNoHp.Margin = new System.Windows.Forms.Padding(2);
+            this.txtNoHp.Name = "txtNoHp";
+            this.txtNoHp.Size = new System.Drawing.Size(181, 20);
+            this.txtNoHp.TabIndex = 3;
+            this.txtNoHp.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNoHp_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 84);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "No.HP";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 107);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Tgl Lahir";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(62, 128);
+            this.txtEmail.Margin = new System.Windows.Forms.Padding(2);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(226, 20);
+            this.txtEmail.TabIndex = 5;
+            this.txtEmail.Leave += new System.EventHandler(this.txtEmail_Leave);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(9, 130);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Email";
+            // 
+            // dtpTglLahir
+            // 
+            this.dtpTglLahir.Location = new System.Drawing.Point(62, 105);
+            this.dtpTglLahir.Margin = new System.Windows.Forms.Padding(2);
+            this.dtpTglLahir.Name = "dtpTglLahir";
+            this.dtpTglLahir.Size = new System.Drawing.Size(181, 20);
+            this.dtpTglLahir.TabIndex = 4;
+            // 
+            // btnSimpan
+            // 
+            this.btnSimpan.Location = new System.Drawing.Point(353, 82);
+            this.btnSimpan.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSimpan.Name = "btnSimpan";
+            this.btnSimpan.Size = new System.Drawing.Size(56, 27);
+            this.btnSimpan.TabIndex = 6;
+            this.btnSimpan.Text = "&Simpan";
+            this.btnSimpan.UseVisualStyleBackColor = true;
+            this.btnSimpan.Click += new System.EventHandler(this.btnSimpan_Click);
+            // 
+            // btnBatal
+            // 
+            this.btnBatal.Location = new System.Drawing.Point(353, 116);
+            this.btnBatal.Margin = new System.Windows.Forms.Padding(2);
+            this.btnBatal.Name = "btnBatal";
+            this.btnBatal.Size = new System.Drawing.Size(56, 27);
+            this.btnBatal.TabIndex = 7;
+            this.btnBatal.Text = "&Batal";
+            this.btnBatal.UseVisualStyleBackColor = true;
+            this.btnBatal.Click += new System.EventHandler(this.btnBatal_Click);
+            // 
+            // FrmTambahData
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 164);
+            this.Controls.Add(this.btnBatal);
+            this.Controls.Add(this.btnSimpan);
+            this.Controls.Add(this.dtpTglLahir);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtNoHp);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtKota);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtAlamat);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNama);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmTambahData";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tambah Data";
+            this.Load += new System.EventHandler(this.FrmTambahData_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.TextBox txtAlamat;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtKota;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNoHp;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DateTimePicker dtpTglLahir;
+        private System.Windows.Forms.Button btnSimpan;
+        private System.Windows.Forms.Button btnBatal;
+    }
+}
---
+++ FrmTambahData.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace AddressBookBetter
+{
+    public partial class FrmTambahData : Form
+    {
+        bool _result = false;
+        bool _addMode = false; 
+        string _editData;
+        int _row;
+
+        public bool Run(FrmTambahData form)
+        {
+            form.ShowDialog();
+            return _result;
+        }
+
+        private void FrmTambahData_Load(object sender, EventArgs e)
+        {
+            if (!_addMode) { 
+            string[] arrData = _editData.Split(';');
+            txtNama.Text = arrData[0];
+            txtAlamat.Text = arrData[1];
+            txtKota.Text = arrData[2];
+            txtNoHp.Text = arrData[3];
+            dtpTglLahir.Text = arrData[4];
+            txtEmail.Text = arrData[5];
+            }
+        }
+
+        public FrmTambahData(bool addMode, string editData, int row)
+        {
+            InitializeComponent();
+            _addMode = addMode;
+            _editData = editData;
+            _row = row;
+        }
+
+        private void btnBatal_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtNoHp_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+        private bool EmailIsValid(string emailAddr)
+        {
+            string emailPattern1 = @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$";
+            Regex regex = new Regex(emailPattern1);
+            Match match = regex.Match(emailAddr);
+            return match.Success;
+        }
+
+        private void txtEmail_Leave(object sender, EventArgs e)
+        {
+            if (this.txtEmail.Text.Trim() != "")
+            {
+                if (!EmailIsValid(this.txtEmail.Text))
+                {
+                    MessageBox.Show("Sorry, data E-mail tidak valid...", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    this.txtEmail.Clear();
+                    this.txtEmail.Focus();
+                }
+            }
+        }
+
+        private void btnSimpan_Click(object sender, EventArgs e)
+        {
+            
+            if (txtNama.Text.Trim() == "")
+            {
+                MessageBox.Show("Sorry, nama wajib di isi.");
+                txtNama.Focus();
+            }
+            else if (txtAlamat.Text.Trim() == "")
+            {
+                MessageBox.Show("Sorry, alamat wajib di isi.");
+                txtAlamat.Focus();
+            }
+            else if (txtKota.Text.Trim() == "")
+            {
+                MessageBox.Show("Sorry, kota wajib di isi.");
+                txtKota.Focus();
+            }
+            else if (txtNoHp.Text.Trim() == "")
+            {
+                MessageBox.Show("Sorry, no.hp wajib di isi.");
+                txtNoHp.Focus();
+            }
+            else if (txtEmail.Text.Trim() == "")
+            {
+                MessageBox.Show("Sorry, email wajib di isi.");
+                txtEmail.Focus();
+            }
+            else if (txtEmail.Text.Trim() == "") {
+                MessageBox.Show("Sorry, E-mail tidak valid.");
+                txtEmail.Focus();
+            }
+            else
+            {
+                AddressBookController address = new AddressBookController();
+                address.AddItem(_editData.Split(';'), _addMode, _row);
+                
+                this.Close();
+
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNama_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
